af7acbaebb39973e6915375282d77a06a8198a93
david <wdperdigon1227@gmail.com>
2025-02-27 20:01:20 -0500

    feat: add sidebar component for improved navigation layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,18 +3,22 @@ import Navbar from "../ui/navBar/navbar";
 
 // eslint-disable-next-line no-duplicate-imports
 import type { ReactNode } from "react";
+import Sidebar from "../ui/navBar/sidebar";
 
 interface LayoutProps {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
-    return (
-        <div>
-            <Navbar />
-            <main>{children}</main>
-        </div>
-    );
+	return (
+		<div className="flex">
+			<Sidebar />
+			<div className="flex-1">
+				<Navbar />
+				<main className="p-4">{children}</main>
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
